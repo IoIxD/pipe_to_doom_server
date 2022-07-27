2dd4afb0ac3aace8aef9bbc25c5acdea2127177d
IoIxD <alphaproject217@gmail.com>
2022-07-26 20:07:33 -0700

    messages are now buffered to increase speed

--- main.go
+++ main.go
@@ -2,19 +2,26 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/diamondburned/arikawa/v3/api"
 	"github.com/diamondburned/arikawa/v3/discord"
+	"github.com/diamondburned/arikawa/v3/gateway"
 	"github.com/pelletier/go-toml/v2"
 )
 
-var client 		*api.Client
+var g 		*gateway.Gateway
+var client 	*api.Client
+var ctx 	context.Context
+var me 		discord.User
 
+var messageBuffer string
 
 var cfg struct {
 	Token	string
@@ -35,11 +42,28 @@ func main() {
 
 	// Initialize the client
 	client = api.NewClient("Bot "+cfg.Token)
+	me_, err := client.Me() 
+	if(err != nil) {
+		fmt.Println(err)
+		return
+	}
+	me = *me_
 
-	fmt.Println("Online.")
+	// Start listening for events.
+	ctx, _ = signal.NotifyContext(context.Background(), os.Interrupt)
+	g, err = gateway.NewWithIntents(ctx, cfg.Token, gateway.IntentGuilds, gateway.IntentGuildMessages)
+	if err != nil {
+		log.Fatalln("failed to initialize gateway:", err)
+	}
 
 	// set up a go routine for detecting when the program is closed.
 	watchForClose()
+
+	// set up a go routine for detecting when something is typed in the relevant channel
+	go watchForChannelMessages()
+
+	// set up a go routine for watching the message buffer and posting it when it's not empty
+	go watchMessageBuffer()
 
 	// listen to the stdin
 	watchForStdin()
@@ -49,13 +73,24 @@ func watchForClose() {
     sigs := make(chan os.Signal, 1)
     signal.Notify(sigs, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT )
     go func() {
-	    wow := <-sigs
-	    fmt.Println(wow)
+	    <-sigs
 	    Print("Program stopped.")
 	    os.Exit(0)
     }()
-   
+}
 
+func watchForChannelMessages() {
+	for op := range g.Connect(ctx) {
+		switch data := op.Data.(type) {
+		case *gateway.ReadyEvent:
+			fmt.Println("Online.")
+		case *gateway.MessageCreateEvent:
+			if(data.ChannelID != cfg.ChannelID) {continue}
+			if(data.Author == me) {continue}
+			os.Stdout.Write([]byte(data.Content))
+			os.Stdout.Write([]byte("\n"))
+		}
+	}
 }
 
 func watchForStdin() {
@@ -70,7 +105,18 @@ func watchForStdin() {
 		switch message {
 			// By default, just send the output to a channel.
 			default: 
-				Print(message)
+				messageBuffer += message
+		}
+	}
+}
+
+func watchMessageBuffer() {
+	for {
+		time.Sleep(time.Second * 1)
+		if(messageBuffer != "") {
+			fmt.Println(messageBuffer)
+			Print(messageBuffer)
+			messageBuffer = ""
 		}
 	}
 }
